64d2a9697ef40d5ca705307e30c596f4d91c59ad
장기중 <@DESKTOP-ILRL865.local>
2019-11-07 18:53:10 +0900

    commit

---
+++ src/main/java/org/zerock/mapper/BoardMapper.java
@@ -0,0 +1,29 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.zerock.domain.BoardVO;
+//import org.zerock.domain.Criteria;
+import org.zerock.domain.Criteria;
+
+public interface BoardMapper {
+
+	public List<BoardVO> getList();
+
+	public void insert(BoardVO board);
+
+	public Integer insertSelectKey(BoardVO board);
+
+	public BoardVO read(Long bno);
+
+	public int delete(Long bno);
+
+	public int update(BoardVO board);
+
+	public List<BoardVO> getListWithPaging(Criteria cri);
+
+	public int getTotalCount(Criteria cri);
+
+	//public List<BoardVO> getListWithPaging(Criteria cri);
+	//public int getTotalCount(Criteria cri);
+}
